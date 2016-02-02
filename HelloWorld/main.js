a52b0e45f2cce91f75f33bd9f593b52136fefb33
aleckxie <327937132@qq.com>
2016-02-03 00:09:59 +0800

    add folder

---
+++ HelloWorld/main.js
@@ -0,0 +1,18 @@
+//chrome launched the app first time
+chrome.app.runtime.onLaunched.addListener(function (launchData){
+
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 500;
+  var height= 300;
+  // create window
+  chrome.app.window.create("index.html",  {
+  	id : "myWindow1",
+  	outerBounds :{
+  		width : wdith,
+  		height : height,
+        maxWidth : screenWidth,
+        maxHeight : screenHeight
+  	}
+  });
+});
